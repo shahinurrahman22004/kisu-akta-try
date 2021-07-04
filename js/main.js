59b3f8c4e31d9ae639867afb90e3e88ba3b320b8
shahinurrahman22004 <shahinur02345@gmail.como>
2021-07-04 21:34:47 +0600

    js added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function imgSlider(anything){
-    document.querySelector(.starbucks).src = anything;
+    document.querySelector('.starbucks').src = anything;
+}
+
+
+function changeCircleColor (color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
 }
